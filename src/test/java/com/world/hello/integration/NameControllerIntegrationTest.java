f2e24465195eb3c54211140bf75fb0222471943a
xavierllabres1 <xavierllabres1@paucasesnovescifp.cat>
2023-05-02 10:30:26 +0200

    Integration Test

--- src/test/java/com/world/hello/integration/NameControllerIntegrationTest.java
+++ src/test/java/com/world/hello/integration/NameControllerIntegrationTest.java
@@ -9,9 +9,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -20,8 +23,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = NameController.class)
@@ -32,6 +35,7 @@ public class NameControllerIntegrationTest {
 
     @MockBean
     private NameService nameService;
+
 
     @Test
     @DisplayName("Integration - FindAll")
@@ -44,6 +48,7 @@ public class NameControllerIntegrationTest {
 
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
+                .andExpect(view().name("index"))
                 .andExpect(model().attributeExists("nameList"))
                 .andExpect(model().attribute("nameList", hasSize(1)))
                 .andExpect(model().attribute("nameList", hasItem(
@@ -58,26 +63,112 @@ public class NameControllerIntegrationTest {
     }
 
     @Test
+    @DisplayName("Integration - New")
+    public void testNewNameForm() throws Exception {
+
+        mockMvc.perform(get("/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("form"))
+                .andExpect(model().attributeExists("nameForm"))
+                .andExpect(model().attribute("nameForm", hasProperty("id", is(0L))));
+    }
+
+    @Test
     @DisplayName("Integration - Find By Id")
-    public void testFindAById() throws Exception {
-        Name name = new Name.Builder().setId(1L).setFirstName("Jhon").build();
+    public void testFindById() throws Exception {
+        Long id = 1L;
+        Name name = new Name.Builder().setId(id).setFirstName("Jhon").build();
 
         given(nameService.findById(1L)).willReturn(name);
 
-        mockMvc.perform(get("/"))
+        mockMvc.perform(get("/edit/{id}", id))
                 .andExpect(status().isOk())
-                .andExpect(model().attributeExists("nameList"))
-                .andExpect(model().attribute("nameList", hasSize(1)))
-                .andExpect(model().attribute("nameList", hasItem(
-                                allOf(
-                                        hasProperty("id", is(1L)),
-                                        hasProperty("firstName", is("Jhon"))
-                                )
-                        )
-                ))
+                .andExpect(view().name("form"))
+                .andExpect(model().attribute("nameForm", name))
 //                .andDo(MockMvcResultHandlers.print())
         ;
     }
+
+    @Test
+    @DisplayName("Integration - Not Find By Id")
+    public void testNotFindById() throws Exception {
+
+        given(nameService.findById(1L)).willReturn(null);
+
+        mockMvc.perform(get("/edit/{id}", 0L))
+                .andExpect(status().isFound())
+                .andExpect(view().name("redirect:/new"))
+//                .andDo(MockMvcResultHandlers.print())
+        ;
+    }
+
+    @Test
+    @DisplayName("Integration - New Name Submit")
+    void testNewNameSubmit() throws Exception {
+
+        Name name = new Name.Builder()
+                .setId(1L)
+                .setFirstName("John")
+                .build();
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("id", name.getId().toString());
+        formData.add("firstName", name.getFirstName());
+
+        mockMvc.perform(post("/new/submit")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .params(formData))
+                        .andExpect(status().isFound())
+                        .andExpect(view().name("redirect:/"))
+        //                .andDo(MockMvcResultHandlers.print())
+        ;
+    }
+
+    @Test
+    @DisplayName("Integration - Edit Name Submit")
+    void testEditNameSubmit() throws Exception {
+
+        Name name = new Name.Builder()
+                .setId(1L)
+                .setFirstName("John")
+                .build();
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("id", name.getId().toString());
+        formData.add("firstName", name.getFirstName());
+
+        mockMvc.perform(post("/edit/submit")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .params(formData))
+                .andExpect(status().isFound())
+                .andExpect(view().name("redirect:/"))
+        //                .andDo(MockMvcResultHandlers.print())
+        ;
+    }
+
+    @Test
+    @DisplayName("Integration - Delete Name Submit")
+    void testDeleteNameSubmit() throws Exception {
+
+        Name name = new Name.Builder()
+                .setId(1L)
+                .setFirstName("John")
+                .build();
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("id", name.getId().toString());
+        formData.add("firstName", name.getFirstName());
+
+        mockMvc.perform(post("/edit/delete")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .params(formData))
+                .andExpect(status().isFound())
+                .andExpect(view().name("redirect:/"))
+        //                .andDo(MockMvcResultHandlers.print())
+        ;
+    }
+
+
 
 
 }
